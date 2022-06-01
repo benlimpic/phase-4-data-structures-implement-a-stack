ddfe680749ae7d78b1e0e2ca4be10e809b49a9c6
init.defaultBranch <benlimpic@gmail.com>
2022-05-31 21:29:29 -0700

    commit tree 5b0f60815d883bf24dab6124791e8bdfe6e2019c

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,34 @@
-# your code here
+class Stack
+
+    def initialize
+        @stack = []
+    end
+
+    def push(value)
+        @stack.push(value)
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
+    end
+
+    def size
+        @stack.size
+    end
+
+    def empty?
+        @stack.empty?
+    end
+
+    def full?
+        @stack.full?
+    end
+
+    def search(value)
+        @stack.includes?(value)
+    end
+end
